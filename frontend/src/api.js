fe6499456fbc8f9ec125e3cb284b94b34423ad26
Prabin R <prabindhas32@gmail.com>
2026-01-17 17:48:45 +0530

    Hardcode Render URL to fix caching issues

---
+++ frontend/src/api.js
@@ -0,0 +1,46 @@
+﻿import axios from 'axios';
+
+// Always use Render URL - simpler, no confusion
+const API_BASE_URL = 'https://mindcare-hospital-booking.onrender.com/api';
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Debug
+console.log('🚀 Connecting to:', API_BASE_URL);
+
+// Doctor APIs
+export const getDoctors = () => api.get('/doctors/');
+export const getDoctor = (id) => api.get(/doctors//);
+export const createDoctor = (doctorData) => api.post('/doctors/', doctorData);
+export const updateDoctor = (id, doctorData) => api.put(/doctors//, doctorData);
+export const patchDoctor = (id, doctorData) => api.patch(/doctors//, doctorData);
+export const deleteDoctor = (id) => api.delete(/doctors//);
+
+export const getDoctorAvailability = (id, date) => 
+  api.get(/doctors//availability/, { params: { date } });
+
+// Specialization APIs
+export const getSpecializations = () => api.get('/specializations/');
+
+// Appointment APIs
+export const getAppointments = (params = {}) => api.get('/appointments/', { params });
+export const getAppointment = (id) => api.get(/appointments//);
+export const createAppointment = (appointmentData) => api.post('/appointments/', appointmentData);
+export const updateAppointment = (id, appointmentData) => api.put(/appointments//, appointmentData);
+export const patchAppointment = (id, appointmentData) => api.patch(/appointments//, appointmentData);
+export const deleteAppointment = (id) => api.delete(/appointments//);
+
+// Check availability for double booking prevention
+export const checkAvailability = (doctorId, date, time) => 
+  api.post('/check-availability/', {
+    doctor: doctorId,
+    date: date,
+    time: time
+  });
+
+export default api;
