002a571d9e08a4d9f15d95581a1cff8f5851ff90
Prabin R <146511157+prabin2907@users.noreply.github.com>
2026-01-17 18:11:27 +0530

    Updated api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Hardcode the URL for now
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://mindcare-hospital-booking.onrender.com/api';;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
